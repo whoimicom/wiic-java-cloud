0d2aae2cac476626498a235de7511e65e84a42cd
whoimi <mail@whoimi.com>
2021-12-15 13:56:32 +0800

    first commit

---
+++ src/main/java/kim/kim/kkcloud/KkCloudApplication.java
@@ -0,0 +1,13 @@
+package kim.kim.kkcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kim/kim/kkcloud/KkCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.kim.kkcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
