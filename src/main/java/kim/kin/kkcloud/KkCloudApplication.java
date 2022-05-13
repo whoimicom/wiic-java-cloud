838d33d1e68f767214a633fd04ea387dfa98f611
whoimi <mail@whoimi.com>
2022-05-13 10:20:17 +0800

    升级版本

--- src/main/java/kim/kin/kkcloud/KkCloudApplication.java
+++ src/main/java/kim/kin/kkcloud/KkCloudApplication.java
@@ -1,4 +1,4 @@
-package kim.kim.kkcloud;
+package kim.kin.kkcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/kim/kin/kkcloud/KkCloudApplicationTests.java
+++ src/test/java/kim/kin/kkcloud/KkCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package kim.kim.kkcloud;
+package kim.kin.kkcloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
